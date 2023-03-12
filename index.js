f96af1c6b260a6309ba7f3f869ec760ddc8728d7
caseyclinch <clinchcasey@gmail.com>
2023-03-11 23:44:48 -0800

    modifies employee objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name:'casey',
+    address:'hell'
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const a = {...employee}
+    a[key] = value
+    return a
+}   
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const a = {...employee}
+    delete a[key]
+    return a
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
